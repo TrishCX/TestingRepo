5a697a9386b3c41ffda5359b12a90899da46ce32
Ritesh <119659853+TrishCX@users.noreply.github.com>
2023-01-25 17:47:58 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ import cors from "cors";
 const port = 3000;
 import cheerio from "cheerio";
 import got from "got";
+import extractUrls from "extract-urls";
 
 const app = express();
 express.json({
@@ -14,7 +15,75 @@ app.get("/", (req, res) => {
   });
 });
 
-app.get("/hello/:name", async (req, res) => {
+app.get("/video-get/:query", async (req, res) => {
+  const query = req.params.query;
+  const baseURI = `https://www.xtits.com/search/${query}/`;
+  const body = await (await got(baseURI)).body;
+  const $ = cheerio.load(body);
+  const videoInformation = [];
+  $(".wrapper")
+    .find(".wrapper-holder")
+    .find(".container")
+    .find(".main-content")
+    .find("#list_videos_videos_list_search_result")
+    .find(".thumbs-holder")
+    .find(".thumb-item")
+    .map(async (index, element) => {
+      const title = $(element)
+        .find(`a.js-open-popup`)
+        .find(".info-holder")
+        .find("p.title")
+        .text()
+        .trim();
+      const _duration = $(element)
+        .find(`a.js-open-popup`)
+        .find(".img-holder")
+        .find("span.time")
+        .text()
+        .trim();
+      const milliseconds = msToDuration(_duration);
+      for (const result of [{ duration: _duration, ms: milliseconds }]) {
+        if (result?.ms <= 600000) return;
+        const eachVideoThumbnail = $(`span:contains("${result.duration}")`)
+          .parent()
+          .parent()
+          .parent()
+          .find("a.js-open-popup")
+          .find(".img-holder")
+          .find("img")
+          .attr("data-original");
+        const eachVideoHREF = $(`span:contains("${result.duration}")`)
+          .parent()
+          .parent()
+          .parent()
+          .find("a.js-open-popup")
+          .attr("href");
+        return videoInformation.push({
+          uri: eachVideoHREF,
+          thumbnail: eachVideoThumbnail,
+          title,
+          duration: _duration,
+        });
+      }
+    });
+  const finalVideoLook = [];
+  const sortedArray = videoInformation.sort(() => Math.random() - 0.5);
+  const slicedArray = sortedArray.slice(0, 5);
+  for (const data of slicedArray) {
+    const response = await fetchVideo(data.uri);
+    finalVideoLook.push({
+      duration: data.duration,
+      thumbnail: data.thumbnail,
+      title: response.title,
+      uri: response.uri[0],
+    });
+  }
+  return res.send({
+    finalVideoLook,
+  });
+});
+
+app.get("/star-get/:name", async (req, res) => {
   const name = req.params.name;
   const BASE_URI = "https://www.babepedia.com";
   const uri = `https://www.babepedia.com/babe/${name}`;
@@ -275,5 +344,65 @@ Solo:")`).text();
       special: specialMoves,
       lesbian: "No Lesbian.",
     };
+  }
+}
+
+async function fetchVideo(_uri) {
+  const body = (await got(_uri)).body;
+  const $ = cheerio.load(body);
+  const title = $(".wrapper")
+    .find(".wrapper-holder")
+    .find(".main-holder")
+    .find(".container-holder")
+    .find(".content-box")
+    .find(".container")
+    .find(".headline")
+    .find(".title-holder")
+    .find("h1.title")
+    .text()
+    .trim();
+  const videoURI = $(".wrapper")
+    .find(".wrapper-holder")
+    .find(".main-holder")
+    .find(".container-holder")
+    .find(".content-box")
+    .find(".container")
+    .find(".block-content")
+    .find(".content-holder")
+    .find(".player-holder")
+    .find("script")
+    .last()
+    .html();
+  const cleanURI = videoURI?.slice(0, videoURI?.lastIndexOf("video_url") + 1);
+  const uri = extractUrls(cleanURI);
+  const filteredURI = uri.filter(
+    (u, i) => u.endsWith(".mp4/") || u.endsWith(".mp4")
+  );
+  return {
+    uri: filteredURI,
+    title,
+  };
+}
+
+function msToDuration(timeInString) {
+  let milliseconds;
+  if (timeInString.split(":").length === 2) {
+    /* For MM:SS */
+    return (milliseconds =
+      Number(timeInString.split(":")[0]) * 60000 +
+      Number(timeInString.split(":")[1]) * 1000);
+  } else if (timeInString.split(":").length === 3) {
+    /* For HH:MM:SS */
+    return (milliseconds =
+      Number(timeInString.split(":")[0]) * 3600000 +
+      Number(timeInString.split(":")[1]) * 60000 +
+      Number(timeInString.split(":")[2]) * 1000);
+  } else if (timeInString.split(":").length === 4) {
+    /* For DD:HH:MM:SS */
+    return (milliseconds =
+      Number(timeInString.split(":")[0]) * 86400000 +
+      Number(timeInString.split(":")[1]) * 3600000 +
+      Number(timeInString.split(":")[2]) * 60000 +
+      Number(timeInString.split(":")[3]) * 1000);
   }
 }
